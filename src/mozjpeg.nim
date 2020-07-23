9d97b2a9be69d55ffa4c1f473715701cbf437467
bung87 <crc32@qq.com>
2020-07-23 21:26:49 +0800

    o

--- src/mozjpeg.nim
+++ src/mozjpeg.nim
@@ -8,14 +8,14 @@ else:
   const mozjpeg = "mozjpeg.dll"
 
 # 
-{.compile: "mozjpeg/api.c",passc:"-I" & currentSourcePath.parentDir() / "mozjpeg" / "mozjpeg-3.3.1",passL:mozjpeg,passc:"-DJPEG_LIB_VERSION=62 -DBITS_IN_JSAMPLE=8".}
+{.compile: "mozjpeg/api.c",passc:"-DJPEG_LIB_VERSION=80 -DBITS_IN_JSAMPLE=8",passc:"-I" & currentSourcePath.parentDir() / "mozjpeg" / "mozjpeg-3.3.1",passL:mozjpeg.}
 
-proc optimizeJPG*(input_data: string,size:culong,quality:cint,fast_encoding:bool,output: cstring) {.importc:"optimizeJPG",header:"mozjpeg/api.h".}
+proc optimizeJPG*(input_data: cstring,size:culong,quality:cint,fast_encoding:bool,output: cstring) {.importc:"optimizeJPG",header:"mozjpeg/api.h".}
 
 when isMainModule:
   var c = readFile("th.jpeg")
   var o:cstring
   echo c.len.culong
-  optimizeJPG(c,c.len.culong,77.cint,true,o)
-  echo o
+  optimizeJPG(c.cstring,c.len.culong,100.cint,false,o)
+  echo $o
   writeFile("th_out.jpeg", $o)
--- src/mozjpeg/api.c
+++ src/mozjpeg/api.c
@@ -1,30 +1,15 @@
 
 
 #include "api.h"
+#include <stdio.h>
 
 typedef struct _error_mgr {
   struct jpeg_error_mgr pub;
   jmp_buf jb;
 } error_mgr;
 
-static void error_emit_exception(j_common_ptr cinfo)
-{
-  error_mgr *myerr = (error_mgr *)cinfo->err;
-  longjmp(myerr->jb, 1);
-}
 
-
-GLOBAL(struct jpeg_error_mgr *)
-pymozjpeg_error(error_mgr *err) 
-{
-  jpeg_std_error((struct jpeg_error_mgr *)err);
-  err->pub.trace_level = 0;
-  err->pub.error_exit = error_emit_exception;
-
-  return (struct jpeg_error_mgr *)err;
-}
-
-void optimizeJPG(unsigned char* input_data,unsigned long input_data_size,int quality,int fast_encoding,unsigned char* outbuffer)
+void optimizeJPG(const unsigned char* input_data,unsigned long input_data_size,int quality,int fast_encoding,unsigned char* outbuffer)
 {
 //   const unsigned char *input_data;
 //   unsigned long input_data_size;
@@ -37,16 +22,20 @@ void optimizeJPG(unsigned char* input_data,unsigned long input_data_size,int qua
   JSAMPARRAY buffer;
   int row_stride;
   dinfo.global_state = cinfo.global_state = 0;
-  dinfo.err = cinfo.err = pymozjpeg_error(&jerr);
+  dinfo.err = cinfo.err = jpeg_std_error(&jerr.pub);
 
   if(setjmp(jerr.jb)) {
     if (dinfo.global_state != 0)
       jpeg_destroy_decompress(&dinfo);
+    if (cinfo.global_state != 0)
+      jpeg_destroy_compress(&cinfo);
     return ;
   }
 
   jpeg_create_decompress(&dinfo);
   jpeg_mem_src(&dinfo, input_data, input_data_size);
+  fprintf( stdout,"%d\n", input_data_size);
+  fprintf( stdout,"%s\n", input_data);
 
   jpeg_save_markers(&dinfo, JPEG_COM, 0xFFFF);
   
@@ -65,10 +54,10 @@ void optimizeJPG(unsigned char* input_data,unsigned long input_data_size,int qua
   cinfo.data_precision = dinfo.data_precision;
   cinfo.image_width = dinfo.image_width;
   cinfo.image_height = dinfo.image_height;
-
+  fprintf( stdout,"%d\n", dinfo.image_width);
   cinfo.raw_data_in = FALSE;
 
-  jpeg_set_defaults(&cinfo);
+  // jpeg_set_defaults(&cinfo);
 
   if (fast_encoding) {
       jpeg_c_set_int_param(&cinfo, JINT_COMPRESS_PROFILE, JCP_FASTEST);
@@ -76,19 +65,21 @@ void optimizeJPG(unsigned char* input_data,unsigned long input_data_size,int qua
   } else {
       jpeg_c_set_int_param(&cinfo, JINT_COMPRESS_PROFILE, JCP_MAX_COMPRESSION);
   }
-
+  jpeg_set_defaults(&cinfo);
   jpeg_set_quality(&cinfo, quality, TRUE);
   
   jpeg_mem_dest(&cinfo, &outbuffer, &outsize);
   jpeg_start_compress(&cinfo, TRUE);
-
+  fprintf( stdout,"%d\n", outsize);
   row_stride = dinfo.image_width * dinfo.output_components;
   buffer = (*dinfo.mem->alloc_sarray)((j_common_ptr) &dinfo, JPOOL_IMAGE, row_stride, 1);
   while(dinfo.output_scanline < dinfo.output_height) {
     JDIMENSION num_scanlines = jpeg_read_scanlines(&dinfo, buffer, 1);
     jpeg_write_scanlines(&cinfo, buffer, num_scanlines);
   }
-
+  fprintf( stdout,"%s\n", outbuffer);
+  fprintf( stdout,"%d\n", dinfo.output_height);
+  fprintf( stdout,"%d\n", outsize);
   jpeg_finish_decompress(&dinfo);
   jpeg_destroy_decompress(&dinfo);
 
